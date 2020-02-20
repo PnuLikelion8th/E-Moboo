5ff8e08908e7061aba41f79779db238d4f7bdf00
Tedhoon <gt0305@likelion.org>
2020-02-21 01:14:23 +0900

    [추가] 기본 세팅 추가 및 main.html 캠퍼스 맵 위치 마킹

--- pnumap/pnumap/settings.py
+++ pnumap/pnumap/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,4 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = '/emopstatic/'
+
