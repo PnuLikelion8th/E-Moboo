9e8d5ca43ea81435884752f3c7edd40404a22a3b
chichi <jang.chihun@gmail.com>
2020-02-23 02:36:39 +0900

    [수정] DB 연결을 위해 json 참조하는 부분 주석처리후 TEST

--- pnumap/pnumap/settings/deploy.py
+++ pnumap/pnumap/settings/deploy.py
@@ -1,10 +1,10 @@
 from .base import *
 import json
 
-config_secret_production = json.loads(open(CONFIG_SECRET_PRODUCTION_FILE).read())
+# config_secret_production = json.loads(open(CONFIG_SECRET_PRODUCTION_FILE).read())
 
 DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pnumap.com','.amazonaws.com']
 
 WSGI_APPLICATION = 'pnumap.wsgi'
