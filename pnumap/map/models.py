71c572593dfea27e2df9c167bbeb9f9ba67c6c4d
hyerimsn <etchee@likelion.org>
2020-02-23 23:20:35 +0900

    [추가] models.py내용

--- pnumap/map/models.py
+++ pnumap/map/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ReviewData(models.Model):
+    coursename = models.CharField(max_length=100)
+    prof = models.CharField(max_length=50)
+    star = models.DecimalField(max_digits=5, decimal_places=2)
