52e56da0d4ea5f4dd32c9e99b65d61f07b675c93
hyerimsn <etchee@likelion.org>
2020-02-24 16:33:37 +0900

    [수정]크롤링모델과 함수수정

--- pnumap/map/models.py
+++ pnumap/map/models.py
@@ -12,24 +12,24 @@ class Blog(models.Model):
         return self.title
 
 class Building(models.Model):
-    num = models.IntegerField() #건물번호
+    num = models.IntegerField(null=True) #건물번호
 
 class Major(models.Model):
-    building = models.ForeignKey(Building, on_delete=models.CASCADE)
-    major = models.CharField(max_length=50) #학과
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, null=True)
+    major = models.CharField(max_length=50, null=True) #학과
 
 class Professor(models.Model):
-    name = models.CharField(max_length=10) #교수이름
-    major = models.ForeignKey(Major, on_delete=models.CASCADE) #전공
-    status = models.CharField(max_length=10) #직위?직책
+    name = models.CharField(max_length=10, null=True) #교수이름
+    major = models.ForeignKey(Major, null=True, on_delete=models.CASCADE) #전공
+    status = models.CharField(max_length=10, null=True) #직위?직책
 
 class Course(models.Model):
-    prof = models.ForeignKey(Professor, on_delete=models.CASCADE)
-    course = models.CharField(max_length=100)
+    prof = models.ForeignKey(Professor, null=True, on_delete=models.CASCADE)
+    course = models.CharField(max_length=100, null=True)
 
 class ReviewData(models.Model):
-    coursename = models.ForeignKey(Course, on_delete=models.CASCADE)
-    star = models.DecimalField(max_digits=5, decimal_places=2)
+    coursename = models.ForeignKey(Course, null=True, on_delete=models.CASCADE)
+    star = models.DecimalField(max_digits=5, decimal_places=2, null=True)
     review = models.TextField(max_length=300, null=True)
     
     def __str__(self):
