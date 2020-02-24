d33b2cf0c20a4e892c5de3595209840300fd00bd
hyerimsn <etchee@likelion.org>
2020-02-24 13:47:46 +0900

    [추가] 파싱데이터 저장 함수

--- pnumap/map/models.py
+++ pnumap/map/models.py
@@ -6,6 +6,9 @@ class ReviewData(models.Model):
     coursename = models.CharField(max_length=100)
     prof = models.CharField(max_length=50)
     star = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    def __str__(self):
+        return self.coursename
 
 class Blog(models.Model):
     title = models.TextField(max_length=200)
--- pnumap/map/views.py
+++ pnumap/map/views.py
@@ -1,8 +1,72 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BlogForm
-from .models import Blog
+from .models import Blog, ReviewData
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
 
-# Create your views here.
+def reviewdata():
+    browser = webdriver.Chrome(r"C:\Users\USER\Desktop\likelion\likelion8th\E-Moboo\chromedriver_win32\chromedriver.exe")
+    browser.get('https://everytime.kr/login')
+    time.sleep(1)
+
+    login = browser.find_element_by_name('userid')
+    login.send_keys('etcheef')
+    login = browser.find_element_by_name('password')
+    login.send_keys('Rimee202=')
+    login.send_keys(Keys.RETURN)
+
+    browser.find_element_by_css_selector("#menu > li:nth-child(3) > a").click()
+    time.sleep(2)
+
+    SCROLL_PAUSE_TIME = 0.5
+    last_height = browser.execute_script("return document.body.scrollHeight")
+    while True:
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_TIME)
+        new_height = browser.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    reviews = browser.find_elements_by_class_name("article")
+    time.sleep(1)
+    i = 0
+
+    while i < len(reviews):
+        reviews[i].send_keys(Keys.CONTROL + '\n')
+        browser.switch_to.window(browser.window_handles[1])
+        wait = WebDriverWait(browser, 10)
+        wait.until(EC.presence_of_element_located((By.ID, "container")))
+    
+        #여기서부터 정보 가져옴
+        req = browser.page_source
+        soup = BeautifulSoup(req,'html.parser')
+        coursename_list = soup.select("#container > div.side.head > h2")
+        for coursename in coursename_list:
+            a = coursename.text
+        
+        prof_list = soup.select('#container > div.side.head > p:nth-child(2) > span')
+        for prof in prof_list:
+            b = prof.text
+    
+        star_list = soup.select('#container > div.side.article > div.rating > div.rate > span > span.value')
+        for star in star_list:
+            c = star.text
+
+        result = ReviewData.objects.create(coursename=a, prof=b, star=c)
+
+        browser.close()
+        browser.switch_to.window(browser.window_handles[0])
+        i+=1
+        time.sleep(2)
+    return result
+
 def main(request):
     return render(request, 'main.html')
 
