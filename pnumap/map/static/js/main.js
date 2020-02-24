2491e76f9a9fcf22cf8bb9c3fbae4f2b65a75d28
Tedhoon <gt0305@likelion.org>
2020-02-24 17:36:55 +0900

    [추가] main 렌더할 때 맵 가운데로 포커싱

--- pnumap/map/static/js/main.js
+++ pnumap/map/static/js/main.js
@@ -1,3 +1,8 @@
+//처음 보여지는 맵 화면이 가운데로 가도록
+const focus_here = document.getElementById('focus');
+focus_here.focus({preventScroll:false});
+        
+//건물 별 효과
 const btn = document.getElementsByTagName('button');
 
 for(let i=0; i < btn.length; i++){
