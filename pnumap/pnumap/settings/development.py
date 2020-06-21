30b41455097fbadeeefc059b464b843debcf30ca
Tedhoon <gt0305@likelion.org>
2020-06-22 02:22:51 +0900

    sqlite3 연동만

--- pnumap/pnumap/settings/development.py
+++ pnumap/pnumap/settings/development.py
@@ -15,16 +15,16 @@ DB_NAME = config_secret_develop['django']['db']['name']
 DB_USER = config_secret_develop['django']['db']['user']
 DB_HOST = config_secret_develop['django']['db']['host']
 
-DATABASES = {
-                'default': {
-                      'ENGINE': 'django.db.backends.postgresql',
-                      'NAME': DB_NAME,
-                      'USER': DB_USER,
-                      'PASSWORD': DB_PW,
-                      'HOST': DB_HOST,
-                      'PORT': '5432',
-                }
-}
+# DATABASES = {
+#                 'default': {
+#                       'ENGINE': 'django.db.backends.postgresql',
+#                       'NAME': DB_NAME,
+#                       'USER': DB_USER,
+#                       'PASSWORD': DB_PW,
+#                       'HOST': DB_HOST,
+#                       'PORT': '5432',
+#                 }
+# }
 
 
 DATABASES = {
