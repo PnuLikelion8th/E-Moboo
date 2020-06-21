3dd142e907af3144f014030072521b8c087180a2
Tedhoon <gt0305@likelion.org>
2020-06-22 02:21:17 +0900

    sqlite3

--- pnumap/pnumap/settings/development.py
+++ pnumap/pnumap/settings/development.py
@@ -25,3 +25,11 @@ DATABASES = {
                       'PORT': '5432',
                 }
 }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
